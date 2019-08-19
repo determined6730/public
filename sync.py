7310727559b8fcdaa415b15f8e2637fa8387784e
Ubuntu <ubuntu@ip-172-26-14-108.ap-northeast-1.compute.internal>
2019-08-19 07:13:21 +0000

    autocommit

--- sync.py
+++ sync.py
@@ -1,15 +1,18 @@
 import subprocess
 import os
 
-print os.getcwd()
+def exec_command(str):
+    result = subprocess.check_output(str,shell=True)
+    print result
 
-result = subprocess.check_output("git status",shell=True)
-print result
 
 
-if "nothing added to commit but untracked files present" in result:
-    print subprocess.check_output("git add *",shell=True)
-    print subprocess.check_output("git commit -m \"autocommit\"",shell=True)
-    print subprocess.check_output("git push origin master",shell=True)
+re = exec_command("git status")
+
+if "nothing added to commit but untracked files present" in re:
+    print exec_command("git add *")
+    print exec_command("git commit -m \"autocommit\"")
+    print exec_command("git push origin master")
 else:
     print "nothing"
+
